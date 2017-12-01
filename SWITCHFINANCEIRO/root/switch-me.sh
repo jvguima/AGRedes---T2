dd2a37d33332515062830fb4f00ef6fbcc2d42a6
Unknown <jvguima@yahoo.com.br>
2017-12-01 18:41:59 -0200

    The Story so Far
    +Basic Infrastructure setup: All machines up and running, routers, switches and routing tables 
    +LAMP server running
    +Syslog running (NETADM, ROTEADOR1, ROTEADOR2, ROTEADORWIFI, FIREWALL, SERVIDOR)
    +SNMPv2c running (NETADM, ROTEADOR1, ROTEADOR2, ROTEADORWIFI, FIREWALL, SERVIDOR)
    +FIREWALL iptables configured to NAT Translation and to Drop all by default policy, do port 80 redirect to SERVIDOR

---
+++ SWITCHFINANCEIRO/root/switch-me.sh
@@ -0,0 +1,12 @@
+brctl addbr br0
+brctl addif br0 eth0
+brctl addif br0 eth1
+brctl addif br0 eth2
+brctl addif br0 eth3
+brctl addif br0 eth4
+ifconfig eth0 0.0.0.0
+ifconfig eth1 0.0.0.0
+ifconfig eth2 0.0.0.0
+ifconfig eth3 0.0.0.0
+ifconfig eth4 0.0.0.0
+ifconfig br0 10.0.6.10 netmask 255.255.255.0 broadcast 10.0.6.255
